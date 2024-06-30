8b3e90bf196156f49574831488f1c29c4fa47da2
KontonGu <kontonpuku@gmail.com>
2024-06-30 12:58:45 +0000

    crd client udpate

---
+++ pkg/fast-configurator/fastconfigurator.go
@@ -0,0 +1,19 @@
+/*
+ * Created/Modified on Mon May 27 2024
+ *
+ * Author: KontonGu
+ * Copyright (c) 2024 TUM - CAPS Cloud
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+
+package fastconfigurator
+
+const (
+	ClientsIPFile          = "/fastpod/library/GPUClientIP.txt"
+	FastSchedulerConfigDir = "/fastpod/scheduler/config"
+	GPUClientsConfigDir    = "/fastpod/scheduler/gpu_clients"
+)
+
+func Run(device_manager string) {
+
+}
