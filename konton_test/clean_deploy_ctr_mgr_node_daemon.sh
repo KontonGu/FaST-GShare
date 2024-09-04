b91aaeec9b6fd342da7fccbd872c24a45e29c04e
KontonGu <kontonpuku@gmail.com>
2024-09-04 01:40:07 +0000

    fast-gshare readme added

--- konton_test/clean_deploy_ctr_mgr_node_daemon.sh
+++
@@ -1,3 +0,0 @@
-kubectl delete -f ../yaml/fastgshare/fastgshare-node-daemon.yaml
-kubectl delete -f ../yaml/fastgshare/fastpod-controller-manager.yaml
-kubectl delete pod -l fastgshare/role=dummyPod -n kube-system
